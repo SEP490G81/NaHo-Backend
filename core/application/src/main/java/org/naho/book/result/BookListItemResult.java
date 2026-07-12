8bdb9aa53666cea9c66ae377cc26104c736f6283
vuongtruc2824 <vuongtrucwork2004@gmail.com>
2026-07-12 21:58:05 +0700

    init: book data and book cover images data

--- core/application/src/main/java/org/naho/book/result/BookListItemResult.java
+++ core/application/src/main/java/org/naho/book/result/BookListItemResult.java
@@ -1,7 +1,6 @@
 package org.naho.book.result;
 
 import org.naho.book.type.CefrLevel;
-import org.naho.book.type.TopicStatus;
 import org.naho.user.type.JLPTLevel;
 
 public record BookListItemResult(
