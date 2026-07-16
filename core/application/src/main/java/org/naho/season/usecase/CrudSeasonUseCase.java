4fa7ea17cc3a3cf1bc99dc0e9da5a4e84101be25
Nguyễn Trực <truc@MacBook-Pro-cua-TRUC.local>
2026-07-16 09:29:15 +0700

    feature: create next season

--- core/application/src/main/java/org/naho/season/usecase/CrudSeasonUseCase.java
+++ core/application/src/main/java/org/naho/season/usecase/CrudSeasonUseCase.java
@@ -7,6 +7,10 @@ import org.naho.season.port.in.CrudSeasonInputPort;
 import org.naho.season.port.out.SeasonRepositoryPort;
 import org.naho.season.result.SeasonResult;
 
+import org.naho.i18n.message.season.SeasonDetailMessageKey;
+import org.naho.season.exception.SeasonErrorCode;
+import org.naho.shared.exception.ApplicationException;
+
 public class CrudSeasonUseCase implements CrudSeasonInputPort {
 
     private final SeasonRepositoryPort seasonRepositoryPort;
@@ -22,6 +26,13 @@ public class CrudSeasonUseCase implements CrudSeasonInputPort {
 
     @Override
     public SeasonResult createNextSeason(CreateSeasonCommand command) {
+        if(seasonRepositoryPort.existsOverlappingSeason(command)) {
+            throw new ApplicationException(
+                    SeasonErrorCode.SEASON_OVERLAPPING,
+                    SeasonDetailMessageKey.SEASON_CREATION_OVERLAPPING
+            );
+        }
+        
         Integer lastestSeasonNo = seasonRepositoryPort.findLatestSeasonNo();
 
         if (lastestSeasonNo == null) {
