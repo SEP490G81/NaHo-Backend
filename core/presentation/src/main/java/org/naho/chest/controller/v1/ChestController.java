5ac7b443b2f9e8d95752661b4478a25996d5563f
vuongtruc2824 <vuongtrucwork2004@gmail.com>
2026-07-20 23:02:43 +0700

    update: change request body of open chest method

--- core/presentation/src/main/java/org/naho/chest/controller/v1/ChestController.java
+++ core/presentation/src/main/java/org/naho/chest/controller/v1/ChestController.java
@@ -2,13 +2,14 @@ package org.naho.chest.controller.v1;
 
 import lombok.RequiredArgsConstructor;
 import org.naho.chest.command.OpenChestCommand;
-import org.naho.chest.dto.mapper.ChestRequestMapper;
 import org.naho.chest.dto.request.OpenChestRequest;
 import org.naho.chest.port.in.OpenChestInputPort;
 import org.naho.i18n.message.chest.ChestDetailMessageKey;
 import org.naho.shared.annotation.ApiResponseMessage;
+import org.naho.user.result.AccessTokenPayload;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChestController {
 
     private final OpenChestInputPort openChestInputPort;
-    private final ChestRequestMapper chestRequestMapper;
 
     @ApiResponseMessage(message = ChestDetailMessageKey.CHEST_OPEN_SUCCESS)
     @PostMapping("/open")
-    public ResponseEntity<Void> openChest(@RequestBody OpenChestRequest request) {
-        OpenChestCommand command = chestRequestMapper.requestToCommand(request);
+    public ResponseEntity<Void> openChest(
+            @RequestBody OpenChestRequest openChestRequest,
+            @AuthenticationPrincipal AccessTokenPayload payload
+    ) {
+        OpenChestCommand command = new OpenChestCommand(
+                openChestRequest.chestId(),
+                payload.userId()
+        );
         openChestInputPort.openChest(command);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
