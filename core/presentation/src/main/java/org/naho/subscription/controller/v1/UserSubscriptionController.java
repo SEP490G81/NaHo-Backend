535888966c0258c19a436b7b22b32bdfb06e2448
Meocute5104 <nguyendangkhoa5104@gmail.com>
2026-08-12 19:36:11 +0700

    #159 - feature: api get subscription by userId

--- core/presentation/src/main/java/org/naho/subscription/controller/v1/UserSubscriptionController.java
+++ core/presentation/src/main/java/org/naho/subscription/controller/v1/UserSubscriptionController.java
@@ -11,6 +11,7 @@ import org.naho.user.result.AccessTokenPayload;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +36,26 @@ public class UserSubscriptionController {
     ) {
         UserSubscriptionResult result = getActiveSubscriptionInputPort
                 .getUserActiveSubscription(payload.userId());
+
+        UserSubscriptionResponse response = userSubscriptionResponseMapper.resultToResponse(result);
+
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * Lấy gói đăng kí hiện tại của người dùng theo id
+     *
+     * @param {id} la user id
+     * @return SubscriptionPlanResponse
+     */
+
+    @GetMapping("/{userId}")
+    @ApiResponseMessage(message = SubscriptionDetailMessageKey.SUBSCRIPTION_USER_GET_ACTIVE_SUCCESS)
+    public ResponseEntity<UserSubscriptionResponse> getUserActiveSubscriptionPlan(
+            @PathVariable("userId") Long id
+    ) {
+        UserSubscriptionResult result = getActiveSubscriptionInputPort
+                .getUserActiveSubscription(id);
 
         UserSubscriptionResponse response = userSubscriptionResponseMapper.resultToResponse(result);
 
