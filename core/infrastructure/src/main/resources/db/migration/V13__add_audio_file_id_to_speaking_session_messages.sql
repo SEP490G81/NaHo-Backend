492470b9d7c2851be9307843f349dd75bee64a08
Nguyễn Trực <truc@Macbook-Nguyen-Truc.local>
2026-08-13 07:56:39 +0700

    update V1

--- core/infrastructure/src/main/resources/db/migration/V13__add_audio_file_id_to_speaking_session_messages.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE speaking_session_messages
-    ADD COLUMN audio_file_id BIGINT NULL;
-
-ALTER TABLE speaking_session_messages
-    ADD CONSTRAINT FK_SPEAKING_SESSION_MESSAGES_ON_AUDIO_FILE FOREIGN KEY (audio_file_id) REFERENCES files (id);
